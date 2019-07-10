b64558b0486cf9f59498b78e28d8ed7aa60a83f4
Kiyotaka <t10kiyotaka@gmail.com>
2019-07-10 23:55:17 +0900

    Add CSS

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.comopnent.html'
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {
 
