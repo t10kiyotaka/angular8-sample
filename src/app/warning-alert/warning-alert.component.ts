f839c000ca03a00406d45f5c3c594a4823ead4a1
Kiyotaka <t10kiyotaka@gmail.com>
2019-07-12 23:55:45 +0900

    Add property

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 
 @Component({
-  selector: 'app-warning-alert',
+  selector: 'app-warning-alert',　
   template: `<p>This is a warning. You are in danger!</p>`,
   styles: 
   [
